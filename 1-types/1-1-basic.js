07c0d55ee05fa1523b9deac6061930c3b05ce392
gomgomigom <nodam89@naver.com>
2022-05-02 15:24:04 +0900

    1-2-function.ts

---
+++ 1-types/1-1-basic.js
@@ -0,0 +1,19 @@
+{
+    // JavaScr ipt
+    // let
+    var name_1 = 'hello';
+    name_1 = 'hi';
+    // const
+    var age = 5;
+    var num = 3;
+    var str = 'hi';
+    var bool = false;
+    var numb = void 0;
+    numb = undefined;
+    numb = 2;
+    var person = void 0;
+    person = 'hhhh';
+    person = null;
+    // null
+    // unknown
+}
