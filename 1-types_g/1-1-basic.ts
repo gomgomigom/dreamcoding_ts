e40f9fc1af85d5d51e13fed1522c82cb319afdf0
gomgomigom <nodam89@naver.com>
2022-05-03 10:48:36 +0900

    1-types folder download

---
+++ 1-types_g/1-1-basic.ts
@@ -0,0 +1,22 @@
+{
+  // JavaScr ipt
+  // let
+  let name = 'hello';
+  name = 'hi';
+  // const
+  const age = 5;
+
+  const num: number = 3;
+  const str: string = 'hi';
+  const bool: boolean = false;
+  let numb: number | undefined;
+  numb = undefined;
+  numb = 2;
+
+  let person: null | string;
+  person = 'hhhh';
+  person = null;
+
+  // null
+  // unknown
+}
